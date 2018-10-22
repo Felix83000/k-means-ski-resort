5b3af23d730c4c902b9a54f3ea49214da911f6ce
Unknown <felix.herrenschmidt@isen.fr>
2018-10-22 11:05:38 +0200

    Ajout des bases pour réaliser l'algorithm K-means

--- src/edu/isen/fhgd/fft/Main.java
+++ src/edu/isen/fhgd/fft/Main.java
@@ -1,15 +1,11 @@
 package edu.isen.fhgd.fft;
 
 import edu.isen.fhgd.fft.controller.FFTController;
-import edu.isen.fhgd.fft.controller.KmeansController;
 import edu.isen.fhgd.fft.fft.FFT;
-import edu.isen.fhgd.fft.kmeans.Kmeans;
-import edu.isen.fhgd.fft.stationsSki.StationSki;
 import edu.isen.fhgd.fft.vue.Fenetre;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.xml.parsers.ParserConfigurationException;
 
 public class Main {
 
